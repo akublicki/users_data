87648f14a47c9843ea2f3160c934ec71fcacf483
Andrzej Kublicki <pionier2000@wp.pl>
2018-04-22 10:55:50 +0200

    Create objects and prototypes

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
-var peopleDB = [];
+function PersonDB(){
+    this.database = [];
+}
+
+PersonDB.prototype.addPerson = function (person) {
+   this.database.push(person);
+};
+
+PersonDB.prototype.displayPeople = function () {
+    console.log(this.database);
+};
 
 function Person (attributes) {
     this.name = attributes.name;
     this.surname = attributes.surname;
     this.age = attributes.age;
     this.role = attributes.role;
+}
+
+function Render(attribute) {
+    this.container = document.getElementById(attribute);
+}
+
+Render.prototype.setHTML = function (database) {
+    this.container.innerHTML = '';
+    var self = this;
+    database.map(function (person) {
+        self.container.innerHTML += self.renderRow(person.name, person.surname, person.age, person.role);
+    });
 };
 
-function renderRow(name, surname, age, role) {
+Render.prototype.renderRow = function(name, surname, age, role) {
     return '<div><div>' + name + ' </div><div> ' + surname + ' </div><div> ' + age + ' </div><div> ' + role + '</div></div>';
-}
+};
 
-function renderDB() {
-    var containerDB = document.getElementById('records');
-    containerDB.innerHTML = '';
-    peopleDB.map(function (person) {
-        containerDB.innerHTML += renderRow(person.name, person.surname, person.age, person.role);
-    });
-}
+var dbInstance = new PersonDB();
+var renderInstance = new Render('records');
+
+renderInstance.setHTML(dbInstance.database);
 
 var button = document.getElementById('addButton');
 button.addEventListener('click', function(){
@@ -30,8 +49,8 @@ button.addEventListener('click', function(){
         name: inputName,
         surname: inputSurname,
         age: inputAge,
-        role: inputRole,
+        role: inputRole
     });
-    peopleDB.push(person);
-    renderDB();
+    dbInstance.addPerson(person);
+    renderInstance.setHTML(dbInstance.database);
 });
